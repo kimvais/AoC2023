4e7637a18c3b4cc789f666e7c562df5d08c2c8ee
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2023-11-30 13:48:24 +0200

    Initial commit

---
+++ Day1.fs
@@ -0,0 +1,9 @@
+﻿module AoC2023.Day1
+
+let part1 fn () =
+    0L
+    
+let part2 fn () =
+    0L
+    
+
---
+++ Tests.fs
@@ -0,0 +1,17 @@
+﻿module AoC2022.Tests
+
+
+open FsUnit.Xunit
+open Xunit
+
+open AoC2023
+
+[<Fact>]
+let ``day 1, part 1`` () =
+    Day1.part1 "test1" () |> should equal 0L
+    Day1.part1 "1" () |> should equal 0L
+
+[<Fact>]
+let ``day1, part2`` () =
+    Day1.part2 "test1" () |> should equal 0L
+    Day1.part2 "1" () |> should equal 0L
