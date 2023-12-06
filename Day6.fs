cc24b2cb11713be3c6d029bf079e5e50070476c8
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2023-12-06 11:08:34 +0200

    Solve day 6

--- Day6.fs
+++ Day6.fs
@@ -1,7 +1,39 @@
 ﻿module AoC2023.Day6
 
+open System
 open AoC2023.Prelude
 
-let part1 fn () = 0L
 
-let part2 fn () = 0L
+let race (duration, distance) =
+    seq { 1L .. duration - 1L }
+    |> Seq.map (fun pressed -> (duration - pressed) * pressed)
+    |> Seq.filter ((<) distance)
+    |> Seq.length
+
+let part1 fn () =
+    let input =
+        readInput fn
+        |> Seq.map (
+            _.Split(":")
+            >> Seq.last
+            >> _.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        )
+
+    let times = Seq.head input |> Seq.map int64
+    let distances = Seq.last input |> Seq.map int64
+    Seq.zip times distances |> Seq.map race |> Seq.reduce (*)
+
+let part2 fn () =
+    let input =
+        readInput fn
+        |> Seq.map (
+            (_.Split(":")
+             >> Seq.last
+             >> _.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+             >> String.concat "")
+            >> int64
+        )
+
+    let time = Seq.head input
+    let distance = Seq.last input
+    race (time, distance)
