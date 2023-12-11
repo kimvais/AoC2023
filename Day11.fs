a7856d04fc36aff18e95726bf8cf2cbab9f94cc7
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2023-12-11 16:54:05 +0200

    Solve day 11 part 2

--- Day11.fs
+++ Day11.fs
@@ -2,18 +2,23 @@
 
 open AoC2023.Prelude
 
-type Galaxy = { Row: int; Col: int }
+type Galaxy = { Row: int64; Col: int64 }
 
 let containsNoGalaxies cs = cs |> Seq.forall ((=) '.')
 
 
-let expand rows =
-    rows
-    |> Seq.collect (fun row ->
-        if containsNoGalaxies row then
-            seq [ row; row ]
-        else
-            seq [ row ])
+let expand factor emptyRows emptyCols galaxies =
+    galaxies |> List.map (fun g ->
+        let row = emptyRows |> Seq.takeWhile (fun r -> r < g.Row) |> Seq.length |> fun n ->
+            match n with
+            | 0 -> g.Row
+            | x -> int64 (x*factor) + g.Row - int64 x
+        let col = emptyCols |> Seq.takeWhile (fun c -> c < g.Col) |> Seq.length |> fun n ->
+            match n with
+            | 0 -> g.Col
+            | y -> int64 (y*factor) + g.Col - int64 y
+        {Row=row; Col=col}
+        ) 
 
 let rec getAllPairs (pairs: (Galaxy * Galaxy) list) (galaxies: Galaxy list) =
     match galaxies with
@@ -27,27 +32,38 @@ let getDistance (g1, g2) =
     let colD = Seq.max [ g1.Col; g2.Col ] - Seq.min [ g1.Col; g2.Col ]
     rowD + colD
 
-let part1 fn () =
+let solve expandFactor fn () =
     let rows = readInput fn
-    let rows' = expand rows
-    let columns = Seq.transpose rows'
-    let columns' = expand columns
-    let data = Seq.transpose columns'
     // print2d data
 
     let galaxies =
-        data
+        rows
         |> Seq.mapi (fun r row ->
             row
             |> Seq.mapi (fun c ch ->
                 match ch with
-                | '#' -> Some { Row = r; Col = c }
+                | '#' -> Some { Row = int64 r; Col = int64 c }
                 | _ -> None)
             |> Seq.choose id)
         |> Seq.concat
         |> List.ofSeq
 
-    let galaxyPairs = getAllPairs List.empty<Galaxy * Galaxy> galaxies
-    galaxyPairs |> Seq.map getDistance |> Seq.sum |> int64
+    let emptyRows =
+        rows
+        |> Seq.mapi (fun i g -> i, g)
+        |> Seq.filter (snd >> containsNoGalaxies)
+        |> Seq.map (fst >> int64)
 
-let part2 fn () = 0L
+    let emptyColumns =
+        Seq.transpose rows
+        |> Seq.mapi (fun i g -> i, g)
+        |> Seq.filter (snd >> containsNoGalaxies)
+        |> Seq.map (fst >> int64)
+   
+    let galaxies' = expand expandFactor emptyRows emptyColumns galaxies 
+    let galaxyPairs = getAllPairs List.empty<Galaxy * Galaxy> galaxies'
+    galaxyPairs |> Seq.map getDistance |> Seq.sum 
+
+let part1 fn () = solve 2 fn ()
+
+let part2 fn () = solve 1000000 fn ()
