ece1bfd7e2e5773394bb89dcdbe69299cb2e385d
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2023-12-14 19:55:52 +0200

    Let's try again with lists ...

--- Day14.fs
+++ Day14.fs
@@ -23,30 +23,30 @@ let getSymbol =
         | Empty -> "."
         
 let printMap rocks =
-    rocks |> Seq.iter (fun r -> String.concat "" (r |> Seq.map getSymbol) |> printfn "%s")
+    rocks |> List.iter (fun r -> String.concat "" (r |> List.map getSymbol) |> printfn "%s")
 
 [<TailCall>]
-let rec roll' (accumulated: Tile seq, remaining: Tile seq) =
-    match Seq.length remaining with
+let rec roll' (accumulated: Tile list, remaining: Tile list) =
+    match List.length remaining with
     | 0 -> accumulated
-    | 1 -> Seq.append accumulated remaining
+    | 1 -> List.append accumulated remaining
     | _ ->
-        let head = Seq.head remaining
+        let head = List.head remaining
         match head with
-        | Cube -> roll' (Seq.append accumulated [head], Seq.tail remaining)
+        | Cube -> roll' (List.append accumulated [head], List.tail remaining)
         | _ ->
-            let newRolled = Seq.takeWhile ((<>) Cube) remaining |> Seq.sort
-            roll' (Seq.append accumulated newRolled, Seq.skip (Seq.length newRolled) remaining)
+            let newRolled = List.takeWhile ((<>) Cube) remaining |> List.sort
+            roll' (List.append accumulated newRolled, List.skip (List.length newRolled) remaining)
 
 let roll line =
-    roll' (Seq.empty<Tile>, line)
+    roll' (List.empty<Tile>, line)
    
 // let roll = memoize roll'
 
-let rollWest = Seq.map roll
-let rollEast = Seq.map (Seq.rev >> roll >> Seq.rev)
-let rollNorth = Seq.transpose >> Seq.map roll >> Seq.transpose
-let rollSouth = Seq.transpose >> Seq.map (Seq.rev >> roll >> Seq.rev) >> Seq.transpose
+let rollWest = List.map roll
+let rollEast = List.map (List.rev >> roll >> List.rev)
+let rollNorth = List.transpose >> List.map roll >> List.transpose
+let rollSouth = List.transpose >> List.map (List.rev >> roll >> List.rev) >> List.transpose
 
 let spinOnce rocks =
     rocks |> rollNorth |> rollWest |> rollSouth |> rollEast
@@ -62,7 +62,7 @@ let rec spin until rounds rocks =
  
 let solve rounds fn () =
     let input = readInput fn
-    let rocks = input |> Seq.map (Seq.map getShape)
+    let rocks = input |> List.ofSeq |> List.map (Seq.map getShape >> List.ofSeq)
     spin rounds 0 rocks |> printMap
     0L
 
