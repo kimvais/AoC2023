465187f4e8958553ba3ceb2be52fecaaeb5b9eed
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2023-12-14 21:56:46 +0200

    Major rewrite for day 14 part 2 ... again, hopefully this leads to something performant

--- Day14.fs
+++ Day14.fs
@@ -3,91 +3,103 @@
 open System.Threading
 open AoC2023.Prelude
 
-type Shape =
+type Tile =
     | Round
     | Cube
+    | Empty
 
-type Direction = North|South|East|West
+type Direction =
+    | North
+    | South
+    | East
+    | West
 
-type Rock = { Shape: Shape; Row: int; Col: int }
 
 let getShape =
     function
-    | 'O' -> Some Round
-    | '#' -> Some Cube
-    | _ -> None
+    | 'O' -> Round
+    | '#' -> Cube
+    | _ -> Empty
 
-let getLoad dim rock =
-    match rock.Shape with
-    | Round -> dim - rock.Row |> int64
-    | _ -> 0L
+let getSymbol =
+    function
+    | Round -> "O"
+    | Cube -> "#"
+    | Empty -> "."
 
-let isInCol col rock =
-    rock.Col = col
+type Field = { Blocks: (int * int) array; MaxRow: int; MaxCol: int }
 
-let isInRow row rock =
-    rock.Row = row
-    
-let getRocksInColumn rocks c =
-    let rocks' = rocks |> Seq.filter (isInCol c) |> Seq.sortBy _.Row |> List.ofSeq
-    c,rocks'
-    
-let getRocksInRow rocks r =
-    let rocks' = rocks |> Seq.filter (isInRow r) |> Seq.sortBy _.Col |> List.ofSeq
-    r,rocks'
+let printMap rocks =
+    rocks
+    |> Seq.iter (fun r -> String.concat "" (r |> Seq.map getSymbol) |> printfn "%s")
 
-let organize direction rocks =
-    let cols = rocks |> Seq.map _.Col |> Seq.max
-    let rows = rocks |> Seq.map _.Row |> Seq.max
-    match direction with
-    |North ->
-        seq [0..cols] |> Seq.map (getRocksInColumn rocks) |> Map.ofSeq
-    |South ->
-        seq [0..cols] |> Seq.map (getRocksInColumn rocks) |> Seq.rev |> Map.ofSeq
-    |East ->
-        seq [0..rows] |> Seq.map (getRocksInRow rocks) |> Map.ofSeq
-    |West ->
-        seq [0..rows] |> Seq.map (getRocksInRow rocks) |> Seq.rev |> Map.ofSeq
-        
-let rec rollColumn dir moved remaining =
-     let increment =
-         match dir with
-         |North|East -> 1
-         |South|West -> -1
-     let getPos =
-         match dir with
-         |North|South -> (function r -> r.Row)
-         |East|West -> (function r -> r.Col)
-     match remaining with
-         | [] -> moved
-         | head :: tail ->
-             let row = moved |> Seq.tryLast |> function
-                 | Some r -> getPos r + increment
-                 | None -> 0 
-             let rock =
-                 match head.Shape with
-                 | Round -> {head with Row=row}
-                 | Cube -> head
-             rollColumn North (moved @ [rock]) tail
-         
-let roll dir rockmap =
-    rockmap |> Map.map (fun _ rocks -> rollColumn dir List.empty<Rock> rocks)
-    
+let getCoordsByType t rocks =
+    rocks
+    |> Seq.mapi (fun r xs -> xs |> Seq.mapi (fun c rock -> rock, (r, c)))
+    |> Seq.concat
+    |> Seq.filter (fst >> (=) t)
+    |> Seq.map snd
+
+let byRow xs =
+    xs
+    |> Seq.groupBy fst
+    |> Seq.map (fun (r, cs) -> r, cs |> Seq.map snd |> List.ofSeq)
+    |> Map.ofSeq
+
+let byColumn xs =
+    xs
+    |> Seq.groupBy snd
+    |> Seq.map (fun (c, rs) -> c, rs |> Seq.map fst |> List.ofSeq)
+    |> Map.ofSeq
+
+let massage map =
+    map
+    |> Map.toSeq
+    |> Seq.map (fun (row, xs) -> xs |> Seq.map (fun (col, t) -> (row, col), t))
+    |> Seq.concat
+    |> Map.ofSeq
+
+let merge m1 m2 = Map.fold (fun acc key value -> Map.add key value acc) m1 m2
+
+let printField field rocks =
+    let rocks' =
+        rocks
+        |> byRow
+        |> Map.map (fun _ xs -> xs |> List.map (fun x -> x, Round)) |> massage
+
+    let blocks =
+        field.Blocks
+        |> byRow
+        |> Map.map (fun _ xs -> xs |> List.map (fun x -> x, Cube)) |> massage
+
+    let items =
+        (merge rocks' blocks)
+
+    seq [ 0 .. field.MaxRow ]
+    |> Seq.iter (fun row ->
+        seq [ 0 .. field.MaxCol]
+        |> Seq.iter (fun col ->
+            let sym =
+                match items |> Map.tryFind (row, col) with
+                | Some s -> getSymbol s
+                | None -> getSymbol Empty
+
+            printf $"%s{sym}")
+
+        printfn "")
+
 let part1 fn () =
     let input = readInput fn
-    let height = Seq.length input
-    let rocks =
-        input
-        |> Seq.mapi (fun row arr ->
-            arr
-            |> Seq.mapi (fun col c ->
-                match getShape c with
-                | Some s -> Some { Shape = s; Row = row; Col = col }
-                | None -> None))
-        |> Seq.concat
-        |> Seq.choose id
-        |> (organize North)
-    
-    rocks |> roll North |> Map.values |> Seq.concat |> Seq.map (getLoad height) |> Seq.sum
+    let data = input |> Seq.map (Seq.map getShape)
+    let immovables = data |> getCoordsByType Cube |> Array.ofSeq
+    let rocks = data |> getCoordsByType Round
+
+    let field =
+        { Blocks = immovables
+          MaxRow = Seq.length data - 1
+          MaxCol = (Seq.length (Seq.head data) - 1)}
+
+    printField field rocks
+    0L
 
 let part2 fn () = 0L
