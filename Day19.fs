d0c29b42a6d37da6b072ff02637add69732a004d
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2023-12-19 10:59:17 +0200

    Types and parsing for day 19

---
+++ Day19.fs
@@ -0,0 +1,85 @@
+﻿module AoC2023.Day19
+
+open System.Text.RegularExpressions
+open Prelude
+
+type Category =
+    | Looks
+    | Musicality
+    | Aerodynamics
+    | Shininess
+
+type Item =
+    { Looks: int64
+      Musicality: int64
+      Aerodynamics: int64
+      Shininess: int64 }
+
+type Comparison =
+    | Gt of Category * int64
+    | Lt of Category * int64
+
+type Destination =
+    | Reject
+    | Accept
+    | Workflow of string
+
+and Rule = { Test: Comparison option; Dest: Destination }
+
+and Workflow = { Label: string; Rules: Rule list }
+
+let itemRx =
+    Regex(@"\{x=(?<x>\d+),m=(?<m>\d+),a=(?<a>\d+),s=(?<s>\d+)\}")
+
+let workFlowRx = Regex(@"(?<label>[a-z]+)\{(?<rules>.*)\}")
+
+let getCat =
+    function
+    | "x" -> Looks
+    | "m" -> Musicality
+    | "a" -> Aerodynamics
+    | "s" -> Shininess
+
+let parseRule =
+    function
+    | Regex @"([axms])>(\d+):R" [ c; v ] -> { Test = Some(Gt(getCat c, int64 v)); Dest = Reject }
+    | Regex @"([axms])>(\d+):A" [ c; v ] -> { Test = Some(Gt(getCat c, int64 v)); Dest = Accept }
+    | Regex @"([axms])<(\d+):R" [ c; v ] -> { Test = Some(Lt(getCat c, int64 v)); Dest = Reject }
+    | Regex @"([axms])<(\d+):A" [ c; v ] -> { Test = Some(Lt(getCat c, int64 v)); Dest = Reject }
+    | Regex @"([axms])>(\d+):([a-z]+)" [ c; v; wf ] -> { Test = Some(Gt(getCat c, int64 v)); Dest = Workflow wf }
+    | Regex @"([axms])<(\d+):([a-z]+)" [ c; v; wf ] -> { Test = Some(Lt(getCat c, int64 v)); Dest = Workflow wf }
+    | Regex @"([a-z]+)" [wf] -> {Test=None; Dest=Workflow wf}
+    | Regex @"A" [] -> { Test = None; Dest = Accept }
+    | Regex @"R" [] -> { Test = None; Dest = Accept }
+
+let parseWorkFlow s =
+    let m = workFlowRx.Match(s)
+    let rules = m.Groups["rules"].Value.Split(',')
+    m.Groups["label"].Value, rules |> Array.map parseRule
+
+let gv (m: Match) (g: string) = int64 m.Groups[g].Value
+
+let parseItem s =
+    let m = itemRx.Match(s)
+    let v = gv m
+
+    { Looks = v "x"
+      Musicality = v "m"
+      Aerodynamics = v "a"
+      Shininess = v "s" }
+
+let part1 fn () =
+    let [| flowInput; itemInput |] = readInputDelimByEmptyLine fn
+
+    let flows =
+        flowInput
+        |> splitByLinefeed
+        |> Array.map parseWorkFlow
+        |> Map.ofArray
+
+    let items = itemInput |> splitByLinefeed |> Array.map parseItem
+    printfn "%A" flows
+    printfn "%A" items
+    0L
+
+let part2 fn () = 0L
--- Program.fs
+++ Program.fs
@@ -44,15 +44,21 @@ let main argv =
     | "14b" -> Day14.part2 "14" ()
     | "15" -> Day15.part1 "15" ()
     | "15b" -> Day15.part2 "15" ()
-    | "18" -> Day18.part1 "18" ()
-    | "18b" -> Day18.part2 "18" ()
     (*
     | "16" -> Day16.part1 "16" ()
     | "16b" -> Day16.part2 "16" ()
     | "17" -> Day17.part1 "17" ()
     | "17b" -> Day17.part2 "17" ()
+    *)
+    | "18" -> Day18.part1 "18" ()
+    | "18b" -> Day18.part2 "18" ()
     | "19" -> Day19.part1 "19" ()
     | "19b" -> Day19.part2 "19" ()
+    (*
+    | "16" -> Day16.part1 "16" ()
+    | "16b" -> Day16.part2 "16" ()
+    | "17" -> Day17.part1 "17" ()
+    | "17b" -> Day17.part2 "17" ()
     | "20" -> Day20.part1 "20" ()
     | "20b" -> Day20.part2 "20" ()
     | "21" -> Day21.part1 "21" ()
@@ -67,7 +73,7 @@ let main argv =
     | "25" -> Day25.part1 "25" ()
     | "25b" -> Day25.part2 "25" ()
     *)
-    | "test" -> Day18.part1 "test18" ()
+    | "test" -> Day19.part1 "test19" ()
     |> printfn "%d"
     stopWatch.Stop()
     printfn "Ran for %0.3f seconds" stopWatch.Elapsed.TotalSeconds
